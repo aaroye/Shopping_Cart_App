a08a6f9e116fc307d7529632dfd4daf3c35bb9c6
Roy Tu <aaroye2@gmail.com>
2022-12-13 03:02:11 +0800

    Add footer, fixed some css

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import Navbar from "./components/Navbar";
 import Shop from "./pages/shop/Shop";
 import Cart from "./pages/cart/Cart";
 import { ShopContextProvider } from "./context/ShopContext";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <ShopContextProvider>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </ShopContextProvider>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2022 Roy Tu</p>
+      <p>
+        Github:{" "}
+        <a href="https://github.com/aaroye/Shopping_Cart_App" target={"_blank"}>
+          link
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
